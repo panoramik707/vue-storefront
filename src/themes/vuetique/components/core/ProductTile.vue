9b92668f539d6c055f94c70735344ec388052ea9
Syed Daniyal Shah <daniyal.shah@softrove.com>
2019-05-28 12:45:55 +0500

    fixed router-link issue with popular categories

--- src/themes/vuetique/components/core/ProductTile.vue
+++ src/themes/vuetique/components/core/ProductTile.vue
@@ -5,14 +5,14 @@
   >
     <router-link
       class="block no-underline product-link"
-      :to="localizedRoute({
+      :to="(!!!product.is_category)? localizedRoute({
         name: product.type_id + '-product',
         params: {
           parentSku: product.parentSku ? product.parentSku : product.sku,
           slug: product.slug,
           childSku: product.sku
         }
-      })"
+      }) : localizedRoute({ name: 'category', params: { id: product.id, slug: product.slug }})"
       data-testid="productLink"
     >
       <div
@@ -113,7 +113,7 @@ export default {
   },
   methods: {
     onProductPriceUpdate (product) {
-      if (product.sku === this.product.sku) {
+      if (product.sku === this.product.sku && !this.product.is_category) {
         Object.assign(this.product, product)
       }
     },
--- src/themes/vuetique/pages/Home.vue
+++ src/themes/vuetique/pages/Home.vue
@@ -15,7 +15,7 @@
       </div>
     </section> -->
 
-    <section class="new-collection container mb-16" v-if="!!trendingCollection && trendingCollection.length > 0">
+    <section class="new-collection container mb-16" style="margin-top: 100px;" v-if="!!trendingCollection && trendingCollection.length > 0">
       <div>
         <header class="mb-6">
           <h2 class="text-h1 leading-h1 text-center">{{ $t('Trending') }}</h2>
@@ -129,24 +129,10 @@ export default {
     return new Promise((resolve, reject) => {
       Logger.info('Calling asyncData in Home (theme)')()
 
-      let newProductsQuery = prepareQuery({ queryConfig: 'newProducts' })
-      let salesQuery = prepareQuery({ queryConfig: 'inspirations' })
       let trendingQuery = prepareQuery({ queryConfig: 'trending' })
       let popularQuery = prepareQuery({ queryConfig: 'popular' })
 
       store.dispatch('category/list', { includeFields: config.entities.optimize ? config.entities.category.includeFields : null }).then((categories) => {
-        store.dispatch('product/list', {
-          query: trendingQuery,
-          size: 8,
-          sort: 'created_at:desc',
-          includeFields: config.entities.optimize ? (config.products.setFirstVarianAsDefaultInURL ? config.entities.productListWithChildren.includeFields : config.entities.productList.includeFields) : []
-        }).catch(err => {
-          reject(err)
-        }).then((res) => {
-          if (res) {
-            store.state.homepage.trending_collection = res.items
-          }
-        })
         store.dispatch('product/list', {
           query: popularQuery,
           size: 8,
@@ -162,8 +148,11 @@ export default {
               (item) => {
                 let categoryId = item.category_ids.find(categoryId => concernedCategoriesIds.includes(parseInt(categoryId)))
                 if (!!categoryId && items.find(product => product.id === item.id) === undefined) {
+                  item.id = parseInt(categoryId)
+                  item.slug = categories.items.find(category => category.id === parseInt(categoryId)).slug
                   item.name = categories.items.find(category => category.id === parseInt(categoryId)).name
                   item.hide_price = true
+                  item.is_category = true
                   let category = {}
                   Object.assign(category, item)
                   items.push(category)
@@ -172,9 +161,11 @@ export default {
             )
             store.state.homepage.popular_collection = items
           }
+          return resolve()
         })
+
         store.dispatch('product/list', {
-          query: newProductsQuery,
+          query: trendingQuery,
           size: 8,
           sort: 'created_at:desc',
           includeFields: config.entities.optimize ? (config.products.setFirstVarianAsDefaultInURL ? config.entities.productListWithChildren.includeFields : config.entities.productList.includeFields) : []
@@ -182,23 +173,9 @@ export default {
           reject(err)
         }).then((res) => {
           if (res) {
-            store.state.homepage.new_collection = res.items
+            store.state.homepage.trending_collection = res.items
           }
-          store.dispatch('product/list', {
-            query: salesQuery,
-            size: 12,
-            sort: 'created_at:desc',
-            includeFields: config.entities.optimize ? (config.products.setFirstVarianAsDefaultInURL ? config.entities.productListWithChildren.includeFields : config.entities.productList.includeFields) : []
-          }).then((res) => {
-            if (res) {
-              store.state.homepage.sales_collection = res.items
-            }
-            return resolve()
-          }).catch(err => {
-            reject(err)
-          })
-        }).catch(err => {
-          reject(err)
+          return resolve()
         })
       }).catch(err => {
         reject(err)
